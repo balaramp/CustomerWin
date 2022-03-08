efcd9ead4671a6107038f725daf3fc1567022461
bpalasamudrum <30477142+bpalasamudrum@users.noreply.github.com>
2022-03-08 15:55:04 -0500

    Add project files.

---
+++ CustomerWin/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+CustomersInfo.CustomerInfo CustomerInfo = new CustomersInfo.CustomerInfo();
+CustomerInfo.Init();
+Console.WriteLine("Please enter Customer Id:");
+
+var customerPackageInfo  = CustomerInfo.GetCustomerWinInfo(Convert.ToInt32(Console.ReadLine()));
+
+if (customerPackageInfo != null)
+{
+    Console.Write("Customer {0} has ordered {1} package",
+                            customerPackageInfo.Name.Trim(),
+                            customerPackageInfo.Type);
+
+    Console.Write(customerPackageInfo.Won ? " already Won" : " not yet won");
+    Console.WriteLine(customerPackageInfo.EligableToWin ? " and is Eligible to Win" : " and is not Eligible to Win");
+}
+else
+    Console.WriteLine("Customer not exists");
+
+Console.ReadLine();
---
+++ SQLQuery1.sql
@@ -0,0 +1,3 @@
+﻿Select Customers.CustomerId, OrderType, EligibleToWin, Won 
+	from Customers, Orders 
+	where Customers.CustomerId = Orders.CustomerId
